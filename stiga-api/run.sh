b58e682b9d9a168a3567d2ff7ad9857c561c7da1
H2CK <t@jagel.net>
2025-08-25 12:06:25 +0200

    Further fixes
    Signed-off-by: H2CK <t@jagel.net>

--- stiga-api/run.sh
+++ stiga-api/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/with-contenv bashio
+echo "Starting STIGA-API  HA bridge..."
 MQTT_HOST=$(bashio::services mqtt "host")
+MQTT_PORT=$(bashio::services mqtt "port")
+MQTT_PROTOCOL="mqtt://"
+MQTT_DOMAIN=".local.hass.io"
+echo "Using MQTT URL: $MQTT_PROTOCOL$MQTT_HOST$MQTT_DOMAIN$MQTT_PORT"
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
 
@@ -7,4 +12,4 @@ STIGA_USERNAME="$(bashio::config 'stiga_username')"
 STIGA_PASSWORD="$(bashio::config 'stiga_password')"
 
 cd /var/stiga-api/homeassistant
-node ha-mqtt-bridge.js "$STIGA_USERNAME" "$STIGA_PASSWORD" "$MQTT_HOST" "$MQTT_USER" "$MQTT_PASSWORD"
+node ha-mqtt-bridge.js "$STIGA_USERNAME" "$STIGA_PASSWORD" "$MQTT_PROTOCOL$MQTT_HOST$MQTT_DOMAIN$MQTT_PORT" "$MQTT_USER" "$MQTT_PASSWORD"
